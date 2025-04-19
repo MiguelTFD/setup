667b7918b2b40e252d173c1f5136bade6af00259
MiguelTFD <129565082+MiguelTFD@users.noreply.github.com>
2025-04-18 22:02:00 -0500

    feat: add instalation script for ranger devicon plugin

--- autosetup.sh
+++ autosetup.sh
@@ -109,6 +109,9 @@ sudo cp -r "$home_dir/.themes/Gruvbox-Light-hdpi/" /usr/share/themes/
 
 sudo cp -r "$home_dir/.icons/*" /usr/share/icons/
 
+# Ranger devicons plugin install
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+
 curl -sS https://starship.rs/install.sh | sh
 
 echo "Script completado con éxito."
