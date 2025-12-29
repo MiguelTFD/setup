95931d50b18a809181dc528b5f1f2d8b31da824b
MiguelTFD <129565082+MiguelTFD@users.noreply.github.com>
2025-12-29 12:19:28 -0500

    Refactor autosetup.sh with functions for clarity

--- autosetup.sh
+++ autosetup.sh
@@ -19,40 +19,46 @@ install_package() {
         echo "No se pudo instalar $package."
     fi
 }
-
-# Instalar git primero
-echo "Instalando git..."
 install_package git
 
-# Clonar repositorio de dotfiles en el directorio home del usuario actual
-echo "Clonando repositorio de dotfiles..."
-if git clone "$repo_url" "$home_dir/dotfiles" > /dev/null 2>&1; then
-    echo "Repositorio clonado con éxito en $home_dir/dotfiles."
-else
-    echo "No se pudo clonar el repositorio."
-    exit 1
-fi
-
-# Eliminar archivos y directorios en el directorio home que coincidan con los de dotfiles
-echo "Eliminando archivos y directorios en $home_dir que coinciden con los de dotfiles..."
-cd "$home_dir/dotfiles"
-for item in .* *; do
-    if [ -e "$home_dir/$item" ]; then
-        rm -rf "$home_dir/$item"
-        echo "Eliminado $home_dir/$item"
+clone_repo() {
+    echo "Clonando repositorio de dotfiles..."
+    if git clone "$repo_url" "$home_dir/dotfiles" > /dev/null 2>&1; then
+        echo "Repositorio clonado con éxito en $home_dir/dotfiles."
+    else
+        echo "No se pudo clonar el repositorio."
+        exit 1
     fi
-done
+}
+clone_repo
 
-# Mover todo el contenido de dotfiles al directorio home del usuario
-echo "Moviendo el contenido de dotfiles al directorio home..."
-shopt -s dotglob
-mv "$home_dir/dotfiles/"* "$home_dir/"
-shopt -u dotglob
-rm -rf "$home_dir/dotfiles"
-echo "Contenido movido y carpeta dotfiles eliminada."
+remove_default_dotfiles() {
+    echo "Eliminando archivos y directorios en $home_dir que coinciden con los de dotfiles..."
+    
+    cd "$home_dir/dotfiles"
+    for item in .* *; do
+        if [ -e "$home_dir/$item" ]; then
+            rm -rf "$home_dir/$item"
+            echo "Eliminado $home_dir/$item"
+        fi
+    done
+}
+remove_default_dotfiles()
 
-# Lista de paquetes a instalar
-packages=(
+move_repo_dotfiles() {
+    echo "Moviendo el contenido de dotfiles al directorio home..."
+    
+    shopt -s dotglob
+    mv "$home_dir/dotfiles/"* "$home_dir/"
+    
+    shopt -u dotglob
+    rm -rf "$home_dir/dotfiles"
+    
+    echo "Contenido movido y carpeta dotfiles eliminada."
+}
+move_repo_dotfiles
+
+package_list=(
     pulseaudio
     ntfs-3g
     curl
@@ -86,13 +92,12 @@ packages=(
     zathura
     fzf
 )
-# considera cambiar net-tools a network-manager, cuando uses una laptop
+# TODO: considera cambiar net-tools a network-manager, cuando uses una laptop
 
 echo "Instalando paquetes..."
 for pkg in "${packages[@]}"; do
     install_package "$pkg"
 done
-
 echo "Proceso de instalacion de paquetes completado."
 
 sudo cp -r "$home_dir/.themes/Gruvbox-Dark-Medium" /usr/share/themes/
